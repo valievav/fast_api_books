c2234bcad6d592077ca26259412fc9e3ccb20d2d
valievav <valievavm@gmail.com>
2025-04-21 13:39:22 +0300

    Add middleware for custom logging, authorization, CORS & TrustedHost

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,7 @@ from src.auth.routes import auth_router
 from src.books.routes import book_router
 from src.db.main import init_db
 from src.errors import register_all_errors
+from src.middleware import register_middleware
 from src.reviews.routes import review_router
 
 
@@ -27,6 +28,7 @@ app = FastAPI(
     lifespan=life_span,
 )
 register_all_errors(app)
+register_middleware(app)
 
 app.include_router(auth_router, prefix=f'/api/{version}/auth', tags=['auth'])
 app.include_router(book_router, prefix=f'/api/{version}/books', tags=['books'])
---
+++ src/middleware.py
@@ -0,0 +1,55 @@
+import logging
+import time
+
+from fastapi import FastAPI, Request, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.responses import JSONResponse
+
+logger = logging.getLogger('uvicorn.access')
+logger.disabled = True
+
+
+def register_middleware(app: FastAPI):
+
+    @app.middleware('http')
+    async def custom_logging(request: Request, call_next):
+        """
+        Provide custom logging
+        """
+        start_time = time.time()
+
+        response = await call_next(request)
+
+        processed_time = time.time() - start_time
+        message = f'{request.method} - {request.url.path} - {response.status_code} - completed in {processed_time} s'
+        print(message)
+
+        return response
+
+    @app.middleware('http')
+    async def authorization(request: Request, call_next):
+        """
+        Check if 'Authorization' is passed in request headers for API calls
+        """
+        if not "Authorization" in request.headers:
+            return JSONResponse(
+                content={'message': 'Not Authenticated',
+                         'resolution': 'Please provide valid credentials'},
+                status_code=status.HTTP_401_UNAUTHORIZED
+            )
+        response = await call_next(request)
+        return response
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_methods=['*'],
+        allow_headers=['*'],
+        allow_credentials=True,
+    )
+
+    app.add_middleware(
+        TrustedHostMiddleware,
+        allowed_hosts=['localhost', '127.0.0.1']
+    )
